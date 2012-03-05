fb88d6d07c45b4a1374fa0139af9db5fed5fa3c3
Tomi Joki-Korpela <tomij@Tomi-Joki-Korpelas-MacBook-Air.local>
2012-03-05 14:19:09 +0000

    Initial Commit

---
+++ FacebookDevStoryTests/FacebookDevStoryTests.h
@@ -0,0 +1,13 @@
+//
+//  FacebookDevStoryTests.h
+//  FacebookDevStoryTests
+//
+//  Created by Tomi Joki-Korpela on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface FacebookDevStoryTests : SenTestCase
+
+@end
---
+++ FacebookDevStoryTests/FacebookDevStoryTests.m
@@ -0,0 +1,32 @@
+//
+//  FacebookDevStoryTests.m
+//  FacebookDevStoryTests
+//
+//  Created by Tomi Joki-Korpela on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "FacebookDevStoryTests.h"
+
+@implementation FacebookDevStoryTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FacebookDevStoryTests");
+}
+
+@end
